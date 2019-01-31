698942129765f449f34ea78e3d0d3673479fa832
joshua-disney <joshua.a.disney@gmail.com>
2019-01-31 14:19:00 -0700

    Initialized project.

---
+++ friends/src/store/reducers/friendsReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  friends: [],
+  error: null
+}
+
+export const friends = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
